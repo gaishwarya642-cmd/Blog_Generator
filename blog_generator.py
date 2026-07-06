a134eb71691d7507b7c3a675641afacc07876159
gaishwarya642-cmd <gaishwarya642@gmail.com>
2026-07-06 18:41:48 +0530

    Add files via upload

---
+++ blog_generator.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from prompt import blog_prompt
+
+load_dotenv()
+
+def generate_blog(topic, tone, length, audience, temperature):
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        api_key=os.getenv("GOOGLE_API_KEY"),
+        temperature=temperature,
+    )
+
+    prompt = blog_prompt.format(
+        topic=topic,
+        tone=tone,
+        length=length,
+        audience=audience
+    )
+
+    response = llm.invoke(prompt)
+    return response.content
